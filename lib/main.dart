4b0d256f2308d88c9e8cc2a0ab05c90288f0bd03
ruanwiller <ruansilvx@gmail.com>
2024-10-05 19:19:12 -0300

    feat: change background color on tap

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  Color color = Colors.white;
+
+  void changeColor() {
+    setState(() {
+      color = Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      home: GestureDetector(
+        onTap: changeColor,
+        child: Scaffold(
+          backgroundColor: color,
+          body: const Center(
+            child: Text('Hello there'),
+          ),
         ),
       ),
     );
