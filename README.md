88594ea4e8ad5726de7993bfed6e618f5b5b3f10
ruanwiller <ruansilvx@gmail.com>
2024-10-08 21:58:26 -0300

    docs: add documentation

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
-# hello_there
+# Hello there app
 
-A new Flutter project.
+This Flutter application features an interactive and visually appealing color-changing animation. 
+When the user taps anywhere on the screen, a circle expands from the tap point to cover the entire 
+screen, changing the background color. Simultaneously, the center text spins during the animation, 
+creating a dynamic visual effect.
+
+## Features
+
+- **Random Color Generation**: Generates a new random color on each tap.
+- **Smooth Color Transition**: Utilizes an ease-in-out curve for smoother color transitions.
+- **Animated Text**: The center text rotates while the color transition occurs.
+
+## Getting Started
+
+To run this project, ensure you have Flutter installed and set up on your machine.
+
+### Prerequisites
+
+- [Flutter SDK](https://flutter.dev/docs/get-started/install)
+- [Dart SDK](https://dart.dev/get-dart)
+
+### Installation
+
+1. **Clone the repository**:
+    ```sh
+    git clone https://github.com/ruansilvx/hello_there.git
+    ```
+
+2. **Navigate to the project directory**:
+    ```sh
+    cd hello_there
+    ```
+
+3. **Install the dependencies**:
+    ```sh
+    flutter pub get
+    ```
+
+### Running the App
+
+To run the application, use the following command:
+
+```sh
+flutter run
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ void main() {
   runApp(const MainApp());
 }
 
+/// Starting point of the application.
 class MainApp extends StatefulWidget {
+  /// Default constructor
   const MainApp({super.key});
 
   @override
@@ -14,33 +16,46 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
+  /// Controls all animations
   late final AnimationController controller = AnimationController(
     duration: const Duration(seconds: 1),
     vsync: this,
   );
 
+  /// Applies curves to make animation smoother
   late final Animation<double> curvedAnimation = CurvedAnimation(
     parent: controller,
     curve: Curves.easeInOut,
   );
 
-  Color color = Colors.white;
-  Color establishedColor = Colors.white;
+  /// New color for when user taps the screen
+  Color newColor = Colors.white;
+
+  /// Current background color
+  Color currentColor = Colors.white;
+
+  /// Tap position offset to animate new color circle
   Offset startPoint = Offset.zero;
 
+  /// Captures the tap position offset and provides a new color to animate from
   void changeColor(TapDownDetails tapDetails) {
+    final randomizedColor = (Random().nextDouble() * 0xFFFFFF).toInt();
     setState(() {
       startPoint = tapDetails.localPosition;
-      color = Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1);
+      // The random formula also randomizes the opacity, so we manually make it
+      // opaque
+      newColor = Color(randomizedColor).withOpacity(1);
     });
     controller.reset();
     controller.forward();
   }
 
+  /// Updates the background with the new color once the animation is over.
+  /// The background won't change otherwise.
   void updateBackgroundListener(AnimationStatus status) {
     if (status.isCompleted) {
       setState(() {
-        establishedColor = color;
+        currentColor = newColor;
       });
     }
   }
@@ -64,37 +79,14 @@ class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
       home: GestureDetector(
         onTapDown: changeColor,
         child: Scaffold(
-          backgroundColor: establishedColor,
           body: CustomPaint(
             painter: BackgroundCustomPainter(
-              color: color,
+              currentColor: currentColor,
+              newColor: newColor,
               startPoint: startPoint,
               animation: curvedAnimation,
             ),
-            child: Center(
-              child: AnimatedBuilder(
-                animation: curvedAnimation,
-                builder: (context, _) {
-                  return Transform.rotate(
-                    angle: curvedAnimation.value * 2 * pi,
-                    child: const Text(
-                      'Hello there',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 32,
-                        shadows: [
-                          Shadow(
-                            blurRadius: 2.0,
-                            offset: Offset(1.0, 2.0),
-                          ),
-                        ],
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  );
-                },
-              ),
-            ),
+            child: _AnimatedCenterText(curvedAnimation: curvedAnimation),
           ),
         ),
       ),
@@ -102,20 +94,82 @@ class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
   }
 }
 
+/// The center text of the application.
+/// The text has a spin animation controlled by [curvedAnimation]
+class _AnimatedCenterText extends StatelessWidget {
+  /// The default constructor
+  const _AnimatedCenterText({
+    required this.curvedAnimation,
+  });
+
+  /// Manages the text spin animation
+  final Animation<double> curvedAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: curvedAnimation,
+        builder: (context, _) {
+          return Transform.rotate(
+            angle: curvedAnimation.value * 2 * pi,
+            child: const Text(
+              'Hello there',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                shadows: [
+                  // Shadows give better contrast to the text over most
+                  // backgrounds
+                  Shadow(
+                    blurRadius: 2.0,
+                    offset: Offset(1.0, 2.0),
+                  ),
+                ],
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+/// Draws the background of the application.
+/// [currentColor] is the current background color, it's drawn first covering
+/// the whole viewport.
+/// A circle with color [newColor] and center at [startPoint] is drawn growing
+/// in size according to [animation] until fits the entire viewport, covering
+/// the current background color.
 class BackgroundCustomPainter extends CustomPainter {
+  /// The default constructor. All members required.
   BackgroundCustomPainter({
-    required this.color,
+    required this.currentColor,
+    required this.newColor,
     required this.animation,
     required this.startPoint,
   }) : super(repaint: animation);
 
-  final Color color;
+  /// The current backgroundColor
+  final Color currentColor;
+
+  /// The color of the animated circle
+  final Color newColor;
+
+  /// The center point of the animated circle
   final Offset startPoint;
+
+  /// The animation values to animate the circle
   final Animation<double> animation;
 
   @override
   void paint(Canvas canvas, Size size) {
-    final paint = Paint()..color = color;
+    canvas.drawColor(currentColor, BlendMode.srcOver);
+
+    final paint = Paint()
+      ..color = newColor;
+
     canvas.drawCircle(
       startPoint,
       (size.longestSide + 50) * animation.value,
@@ -125,6 +179,7 @@ class BackgroundCustomPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(BackgroundCustomPainter oldDelegate) {
-    return oldDelegate.color != color;
+    return oldDelegate.newColor != newColor ||
+        oldDelegate.currentColor != currentColor;
   }
 }
